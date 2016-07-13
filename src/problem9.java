d15aff07a1c81f1c771172efb8bcc2f21cc47f0b
Daniel Son <sondaniel96@gmail.com>
2016-07-13 13:36:00 -0700

    added and edit files

--- src/problem9.java
+++ src/problem9.java
@@ -7,10 +7,15 @@
  There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  Find the product abc.
  */
+
+import java.lang.Math;
+
 public class problem9 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        double a = 1,b = 2,c = 997;
+        final double max = 1000;
+
 
     }
 
